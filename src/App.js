d79294115fdf2783efa3d612998bb125831d8664
Blackwine <daoquocvuong@gmail.com>
2017-11-16 11:39:16 +0700

    Add store

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CreatePostPage from "./pages/createPost/index.js";
 
 import _ from "lodash";
 import "./App.css";
+import { createStore } from "redux";
 
 function generateID() {
   return _.uniqueId("POST_");
@@ -27,6 +28,26 @@ let allPost = [
     content: "This is post 3 content"
   }
 ];
+
+/**
+ * This is a reducer, a pure function with (state, action) => state signature.
+ * It describes how an action transforms the state into the next state.
+ *
+ * The shape of the state is up to you: it can be a primitive, an array, an object,
+ * or even an Immutable.js data structure. The only important part is that you should
+ * not mutate the state object, but return a new object if the state changes.
+ *
+ * In this example, we use a `switch` statement and strings, but you can use a helper that
+ * follows a different convention (such as function maps) if it makes sense for your
+ * project.
+ */
+function reducer(state = allPost, action) {
+  return state;
+}
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(reducer);
 
 class App extends Component {
   constructor() {
