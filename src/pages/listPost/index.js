db70bc3ef6867a299d6672302dbbe6adbb661dce
Blackwine <daoquocvuong@gmail.com>
2017-11-16 15:17:43 +0700

    Connect ListPostPage with redux

--- src/pages/listPost/index.js
+++ src/pages/listPost/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import ListPost from "../../containers/listPost/index.js";
+import { connect } from "react-redux";
 
 import "./index.css";
 
-export default class extends Component {
+class ListPostPage extends Component {
   render() {
     return (
       <div className="list-post-page">
-        <ListPost
-          allPost={this.props.allPost}
-          removePost={this.props.removePost}
-        />
+        <ListPost allPost={[]} removePost={{}} />
       </div>
     );
   }
 }
+
+export default connect()(ListPostPage);
