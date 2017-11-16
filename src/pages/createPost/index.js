4a13d78b1b0f9ce053e4383fdfd9e5f60c95e8e8
Blackwine <daoquocvuong@gmail.com>
2017-11-16 15:32:29 +0700

    Connect CreatePostPage with redux

--- src/pages/createPost/index.js
+++ src/pages/createPost/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import CreatePost from "../../components/createPost/index.js";
 import "./index.css";
-export default class extends Component {
+import { connect } from "react-redux";
+import { createPost } from "../../actions/post";
+
+class CreatePostPage extends Component {
   render() {
     return (
       <div className="create-page">
-        <CreatePost addPost={this.props.addPost} />
+        <CreatePost addPost={this.props.createPost} />
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    createPost: (title, content) => dispatch(createPost(title, content))
+  };
+};
+export default connect(null, mapDispatchToProps)(CreatePostPage);
