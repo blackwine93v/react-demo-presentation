f8ecea362d7e4c0c694bb06574966563b58f3f25
Blackwine <daoquocvuong@gmail.com>
2017-11-20 14:24:26 +0700

    Import lib

--- src/server/server-side-rendering.js
+++ src/server/server-side-rendering.js
@@ -2,6 +2,11 @@ require("ignore-styles");
 
 const express = require("express");
 const app = express();
+const renderToString = require("react-dom/server").renderToString;
+const App = require("../App.js").default;
+const fs = require("fs");
+const path = require("path");
+const React = require("react");
 
 app.use("/static", express.static("build/static"));
 
