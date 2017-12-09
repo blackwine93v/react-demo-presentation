3fd077f013e37d4704bd659373171b35f3130c07
Blackwine93v <daoquocvuong@gmail.com>
2017-12-09 15:23:06 +0700

    Implement router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { Component } from "react";
 import MainLayout from "./layouts/main/MainLayout.js";
 import ListPostPage from "./pages/listPost/index.js";
 import CreatePostPage from "./pages/createPost/index.js";
+import Header from "./components/header/index.js";
 
 import _ from "lodash";
 import "./App.css";
+
+//import react-router
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MainLayout>
-          <ListPostPage />
-          <CreatePostPage />
+          <Router>
+            <div>
+              <Header />
+              <Switch>
+                <Route exact path="/" render={() => <Redirect to="/posts" />} />
+                <Route path="/posts" component={ListPostPage} />
+                <Route path="/newpost" component={CreatePostPage} />
+
+                <Route path="*" render={() => <h1>Not match 404</h1>} />
+              </Switch>
+            </div>
+          </Router>
         </MainLayout>
       </div>
     );
